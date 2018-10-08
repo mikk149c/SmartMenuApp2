e050b070d06f3a4f98a9ab9f1258570784273cff
Mikkel Jørgensen <mikk149c@edu.eal.dk>
2018-10-08 13:05:56 +0200

    Add project files.

---
+++ SmartMenuApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartMenuLibrary;
+
+namespace SmartMenuApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Program myProgram = new Program();
+			myProgram.Run();
+		}
+
+		private void Run()
+		{
+			SmartMenu menu = new SmartMenu();
+			menu.LoadMenu("TestMenu.txt");
+			menu.Activate();
+		}
+	}
+}
