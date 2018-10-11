42bc64f6d9f176acc43fa4bcc6b1880fdf5db733
Mikkel Jørgensen <mikk149c@edu.eal.dk>
2018-10-11 22:14:53 +0200

    Extracted repition to a method in Binding class

--- SmartMenuApp2/Binding.cs
+++ SmartMenuApp2/Binding.cs
@@ -18,63 +18,56 @@ namespace SmartMenuApp2
 			switch (id)
 			{
 				case "Add":
-					tryGetDouble(out num1);
-					Console.WriteLine("+");
-					tryGetDouble(out num2);
-					display(CalculatorClass.Add(num1, num2).ToString());
-					Console.ReadKey();
+					getTwoDoubleWithStringSeperator(out num1, "+", out num2);
+					displayResult(CalculatorClass.Add(num1, num2).ToString());
 					break;
 				case "Subtract":
-					tryGetDouble(out num1);
-					Console.WriteLine("-");
-					tryGetDouble(out num2);
-					display(CalculatorClass.Subtract(num1, num2).ToString());
-					Console.ReadKey();
+					getTwoDoubleWithStringSeperator(out num1, "-", out num2);
+					displayResult(CalculatorClass.Subtract(num1, num2).ToString());
 					break;
 				case "Multiply":
-					tryGetDouble(out num1);
-					Console.WriteLine("x");
-					tryGetDouble(out num2);
-					display(CalculatorClass.Multiply(num1, num2).ToString());
-					Console.ReadKey();
+					getTwoDoubleWithStringSeperator(out num1, "x", out num2);
+					displayResult(CalculatorClass.Multiply(num1, num2).ToString());
 					break;
 				case "Divide":
-					tryGetDouble(out num1);
-					Console.WriteLine("/");
-					tryGetDouble(out num2);
-					display(CalculatorClass.Divide(num1, num2).ToString());
-					Console.ReadKey();
+					getTwoDoubleWithStringSeperator(out num1, "/", out num2);
+					displayResult(CalculatorClass.Divide(num1, num2).ToString());
 					break;
 				case "Sum":
 					doubleArray = getDoubleArray();
-					display(CalculatorClass.Sum(doubleArray).ToString());
-					Console.ReadKey();
+					displayResult(CalculatorClass.Sum(doubleArray).ToString());
 					break;
 				case "Minimum":
 					doubleArray = getDoubleArray();
-					display(CalculatorClass.Minimum(doubleArray).ToString());
-					Console.ReadKey();
+					displayResult(CalculatorClass.Minimum(doubleArray).ToString());
 					break;
 				case "Maximum":
 					doubleArray = getDoubleArray();
-					display(CalculatorClass.Maximum(doubleArray).ToString());
-					Console.ReadKey();
+					displayResult(CalculatorClass.Maximum(doubleArray).ToString());
 					break;
 				case "Average":
 					doubleArray = getDoubleArray();
-					display(CalculatorClass.Average(doubleArray).ToString());
-					Console.ReadKey();
+					displayResult(CalculatorClass.Average(doubleArray).ToString());
 					break;
 				default:
 					break;
 			}
+			if (id != "exit") Console.ReadKey();
+		}
+
+		private void getTwoDoubleWithStringSeperator(out double num1, string seperator, out double num2)
+		{
+			tryGetDouble(out num1);
+			Console.WriteLine(seperator);
+			tryGetDouble(out num2);
 		}
 
 		private bool tryGetDouble(out double num)
 		{
+			ConsoleColor c = Console.ForegroundColor;
 			Console.ForegroundColor = ConsoleColor.Red;
 			bool value = double.TryParse(Console.ReadLine(), out num);
-			Console.ForegroundColor = ConsoleColor.White;
+			Console.ForegroundColor = c;
 			return value;
 		}
 
@@ -90,9 +83,15 @@ namespace SmartMenuApp2
 			return doubleArray;
 		}
 
-		private void display(string str)
+		private void displayResult(string str)
 		{
+			ConsoleColor FC = Console.ForegroundColor;
+			ConsoleColor BC = Console.BackgroundColor;
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.BackgroundColor = ConsoleColor.White;
 			Console.WriteLine($"= {str}");
+			Console.ForegroundColor = FC;
+			Console.BackgroundColor = BC;
 		}
 	}
 }
--- SmartMenuApp2/CalculatorClass.cs
+++ SmartMenuApp2/CalculatorClass.cs
@@ -7,83 +7,83 @@ using System.Threading.Tasks;
 namespace SmartMenuApp2
 {
     class CalculatorClass
-    {
-            public static double Add(double v1, double v2)
-            {
-                double addition = v1 + v2;
-                return addition;
-            }
+	{
+		public static double Add(double v1, double v2)
+		{
+			double addition = v1 + v2;
+			return addition;
+		}
 
-            public static double Subtract(double v1, double v2)
-            {
-                double subtraction = v1 - v2;
-                return subtraction;
-            }
+		public static double Subtract(double v1, double v2)
+		{
+			double subtraction = v1 - v2;
+			return subtraction;
+		}
 
-            public static double Multiply(double v1, double v2)
-            {
-                double multiplication = v1 * v2;
-                return multiplication;
-            }
+		public static double Multiply(double v1, double v2)
+		{
+			double multiplication = v1 * v2;
+			return multiplication;
+		}
 
-            public static double Divide(double v1, double v2)
-            {
-                double divide = v1 / v2;
-                return divide;
-            }
+		public static double Divide(double v1, double v2)
+		{
+			double divide = v1 / v2;
+			return divide;
+		}
 
-            public static double Sum(double[] numbers)
-            {
-                double Sum = 0;
-                for (int i = 0; i < numbers.Length; i++)
-                {
+		public static double Sum(double[] numbers)
+		{
+			double Sum = 0;
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				Sum = numbers[i] + Sum;
+			}
+			return Sum;
+		}
 
-                    Sum = numbers[i] + Sum;
-                }
-                return Sum;
-            }
+		public static double Minimum(double[] numbers)
+		{
+			if (numbers.Length == 0) return 0;
+			double Min = numbers[0];
+			for (int i = 1; i < numbers.Length; i++)
+			{
+				if (Min > numbers[i])
+				{
+					Min = numbers[i];
+				}
+			}
 
-            public static double Minimum(double[] numbers)
-            {
-                double Min = 999999999;
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    if (Min > numbers[i])
-                    {
-                        Min = numbers[i];
-                    }
-                }
+			return Min;
+		}
 
-                return Min;
-            }
+		public static double Maximum(double[] numbers)
+		{
+			double Max = 0;
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				if (i == 0 || Max < numbers[i])
+				{
+					Max = numbers[i];
+				}
+			}
 
-            public static double Maximum(double[] numbers)
-            {
-                double Max = 0;
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    if (i == 0 || Max < numbers[i])
-                    {
-                        Max = numbers[i];
-                    }
-                }
+			return Max;
+		}
 
-                return Max;
-            }
+		public static double Average(double[] numbers)
+		{
+			double res = 0;
+			double Average = 0;
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				res = numbers[i] + res;
+			}
 
-            public static double Average(double[] numbers)
-            {
-                double res = 0;
-                double Average = 0;
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    res = numbers[i] + res;
-                }
+			Average = numbers.Length;
+			Average = res / Average;
 
-                Average = numbers.Length;
-                Average = res / Average;
-
-                return Average;
-            }
-        }
-    }
+			return Average;
+		}
+	}
+}
